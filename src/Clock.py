4264fdc00922e81accf67318da60faf9358d9aff
Cerberus2012 <35246157+Cerberus2012@users.noreply.github.com>
2022-11-11 14:54:46 -0700

    Add files via upload
    Changed scaling for small screens

--- src/Clock.py
+++ src/Clock.py
@@ -31,7 +31,7 @@ class Clock(QWidget):
         painter.drawEllipse(int(self.canvas_horizontal_mid - self.clock_radius), int(self.canvas_vertical_mid - self.clock_radius), int(self.clock_radius * 2), int(self.clock_radius * 2))
 
         self.font = QtGui.QFont()
-        self.font.setPointSize(20)
+        self.font.setPointSize(15)
         painter.setFont(self.font)
         for i in range(12):
             self.hour_angle = ((-2*math.pi*i) + (6 * math.pi)) / 12
@@ -39,10 +39,11 @@ class Clock(QWidget):
             self.start_x = self.canvas_horizontal_mid
             self.start_y = self.canvas_vertical_mid
             
-            self.hour_stop_x = self.start_x + int((0.9 * self.clock_radius) * math.cos(self.hour_angle))
-            self.hour_stop_y = self.start_y - int((0.9 * self.clock_radius) * math.sin(self.hour_angle))
+            self.hour_stop_x = self.start_x + int((0.85 * self.clock_radius) * math.cos(self.hour_angle))
+            self.hour_stop_y = self.start_y - int((0.85 * self.clock_radius) * math.sin(self.hour_angle))
             
-            painter.drawText(self.hour_stop_x, self.hour_stop_y, str(i))
+            # painter.drawText(self.hour_stop_x - 7, self.hour_stop_y + 7, str(i))
+            painter.drawText(self.hour_stop_x - 12, self.hour_stop_y - 12, 25, 18, Qt.AlignCenter, str(i))
     
     def update_hourhand(self, painter, start_x, start_y):
         self.hour_angle = ((-2*math.pi*self.hour) + (6 * math.pi)) / 12
@@ -52,8 +53,8 @@ class Clock(QWidget):
 
     def update_minutehand(self, painter, start_x, start_y):
         self.minute_angle = ((-2*math.pi*self.minute) + (30 * math.pi)) / 60
-        self.minute_stop_x = start_x + int((0.8 * self.clock_radius) * math.cos(self.minute_angle))
-        self.minute_stop_y = start_y - int((0.8 * self.clock_radius) * math.sin(self.minute_angle))
+        self.minute_stop_x = start_x + int((0.7 * self.clock_radius) * math.cos(self.minute_angle))
+        self.minute_stop_y = start_y - int((0.7 * self.clock_radius) * math.sin(self.minute_angle))
         painter.drawLine(start_x, start_y, self.minute_stop_x, self.minute_stop_y)
 
     def paintEvent(self, event):
@@ -64,10 +65,17 @@ class Clock(QWidget):
         painter = QPainter(self)
         painter.setPen(self.pen)
 
-        self.draw_clock(painter)
 
         canvas_horizontal_mid = int(self.container.size().width() / 2)
         canvas_vertical_mid = int(self.container.size().height() / 2)
+
+        print(f"Current radius: {self.clock_radius}")
+        print(f"Current container width: {self.container.size().width()}")
+        print(f"Current container height: {self.container.size().height()}")
+        print(f"Setting clock radius to {self.container.size().width()}")
+        self.set_clock_radius(int(self.container.size().width() * 0.25))
+
+        self.draw_clock(painter)
 
         painter.drawEllipse(int(canvas_horizontal_mid - self.clock_radius), int(canvas_vertical_mid - self.clock_radius), int(self.clock_radius * 2), int(self.clock_radius * 2))
 
