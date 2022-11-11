d2a72284f7583d75a6766910de013e94923e863a
Cerberus2012 <35246157+Cerberus2012@users.noreply.github.com>
2022-11-11 14:14:23 -0700

    Add files via upload
    Cleaned up variables, took out unneeded code

--- src/Clock.py
+++ src/Clock.py
@@ -25,59 +25,55 @@ class Clock(QWidget):
         self.update()
 
     def draw_clock(self, painter):
-        canvas_horizontal_mid = int(self.container.size().width() / 2)
-        canvas_vertical_mid = int(self.container.size().height() / 2)
+        self.canvas_horizontal_mid = int(self.container.size().width() / 2)
+        self.canvas_vertical_mid = int(self.container.size().height() / 2)
 
-        painter.drawEllipse(int(canvas_horizontal_mid - self.clock_radius), int(canvas_vertical_mid - self.clock_radius), int(self.clock_radius * 2), int(self.clock_radius * 2))
+        painter.drawEllipse(int(self.canvas_horizontal_mid - self.clock_radius), int(self.canvas_vertical_mid - self.clock_radius), int(self.clock_radius * 2), int(self.clock_radius * 2))
 
-        font = QtGui.QFont()
-        font.setPointSize(20)
-        painter.setFont(font)
+        self.font = QtGui.QFont()
+        self.font.setPointSize(20)
+        painter.setFont(self.font)
         for i in range(12):
-            hour_angle = ((-2*math.pi*i) + (6 * math.pi)) / 12
+            self.hour_angle = ((-2*math.pi*i) + (6 * math.pi)) / 12
 
-            start_x = canvas_horizontal_mid
-            start_y = canvas_vertical_mid
+            self.start_x = self.canvas_horizontal_mid
+            self.start_y = self.canvas_vertical_mid
             
-            hour_stop_x = start_x + int((0.9 * self.clock_radius) * math.cos(hour_angle))
-            hour_stop_y = start_y - int((0.9 * self.clock_radius) * math.sin(hour_angle))
+            self.hour_stop_x = self.start_x + int((0.9 * self.clock_radius) * math.cos(self.hour_angle))
+            self.hour_stop_y = self.start_y - int((0.9 * self.clock_radius) * math.sin(self.hour_angle))
             
-            painter.drawText(hour_stop_x, hour_stop_y, str(i))
+            painter.drawText(self.hour_stop_x, self.hour_stop_y, str(i))
     
-    def update_hourhand(self, painter, start_x, start_y, ellipse_radius):
-        hour_angle = ((-2*math.pi*self.hour) + (6 * math.pi)) / 12
-        hour_stop_x = start_x + int((0.6 * ellipse_radius) * math.cos(hour_angle))
-        hour_stop_y = start_y - int((0.6 * ellipse_radius) * math.sin(hour_angle))
-        painter.drawLine(start_x, start_y, hour_stop_x, hour_stop_y)
+    def update_hourhand(self, painter, start_x, start_y):
+        self.hour_angle = ((-2*math.pi*self.hour) + (6 * math.pi)) / 12
+        self.hour_stop_x = start_x + int((0.6 * self.clock_radius) * math.cos(self.hour_angle))
+        self.hour_stop_y = start_y - int((0.6 * self.clock_radius) * math.sin(self.hour_angle))
+        painter.drawLine(start_x, start_y, self.hour_stop_x, self.hour_stop_y)
 
-    def update_minutehand(self, painter, start_x, start_y, ellipse_radius):
-        minute_angle = ((-2*math.pi*self.minute) + (30 * math.pi)) / 60
-        minute_stop_x = start_x + int((0.8 * ellipse_radius) * math.cos(minute_angle))
-        minute_stop_y = start_y - int((0.8 * ellipse_radius) * math.sin(minute_angle))
-        painter.drawLine(start_x, start_y, minute_stop_x, minute_stop_y)
+    def update_minutehand(self, painter, start_x, start_y):
+        self.minute_angle = ((-2*math.pi*self.minute) + (30 * math.pi)) / 60
+        self.minute_stop_x = start_x + int((0.8 * self.clock_radius) * math.cos(self.minute_angle))
+        self.minute_stop_y = start_y - int((0.8 * self.clock_radius) * math.sin(self.minute_angle))
+        painter.drawLine(start_x, start_y, self.minute_stop_x, self.minute_stop_y)
 
     def paintEvent(self, event):
         # print("In paint event")
-        pen = QtGui.QPen()
-        pen.setColor(QtGui.QColor("white"))
-        pen.setWidth(2)
+        self.pen = QtGui.QPen()
+        self.pen.setColor(QtGui.QColor("white"))
+        self.pen.setWidth(2)
         painter = QPainter(self)
-        painter.setPen(pen)
+        painter.setPen(self.pen)
 
         self.draw_clock(painter)
-
-        ellipse_diameter = 400
-        ellipse_radius = int(ellipse_diameter / 2)
 
         canvas_horizontal_mid = int(self.container.size().width() / 2)
         canvas_vertical_mid = int(self.container.size().height() / 2)
 
         painter.drawEllipse(int(canvas_horizontal_mid - self.clock_radius), int(canvas_vertical_mid - self.clock_radius), int(self.clock_radius * 2), int(self.clock_radius * 2))
-        # painter.drawEllipse(int(canvas_horizontal_mid - (ellipse_diameter / 2)), int(canvas_vertical_mid - (ellipse_diameter / 2)), ellipse_diameter, ellipse_diameter)
 
-        start_x = canvas_horizontal_mid
-        start_y = canvas_vertical_mid
+        start_x = self.canvas_horizontal_mid
+        start_y = self.canvas_vertical_mid
 
         # painter.drawPoint(canvas_horizontal_mid, canvas_vertical_mid)
-        self.update_hourhand(painter, start_x, start_y, self.clock_radius)
-        self.update_minutehand(painter, start_x, start_y, self.clock_radius)
+        self.update_hourhand(painter, start_x, start_y)
+        self.update_minutehand(painter, start_x, start_y)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 import sys
 from datetime import datetime
 
@@ -15,8 +15,8 @@ def update_hex_clock():
     hour_to_hex = hex(datetime.now().hour)
     minute_to_hex = hex(datetime.now().minute)
 
-    print(f"Hour Dec: {datetime.now().hour} - Hour Hex: {hour_to_hex}")
-    print(f"Minute Dec: {datetime.now().minute} - Minute Hex: {minute_to_hex}")
+    # print(f"Hour Dec: {datetime.now().hour} - Hour Hex: {hour_to_hex}")
+    # print(f"Minute Dec: {datetime.now().minute} - Minute Hex: {minute_to_hex}")
 
     ui.hex_clock_hours_label.setText(hour_to_hex)
     ui.hex_clock_minutes_label.setText(minute_to_hex)
@@ -26,8 +26,8 @@ def update_binary_clock():
     hour_to_bin = bin(datetime.now().hour)
     minute_to_bin = bin(datetime.now().minute)
 
-    print(f"Hour Dec: {datetime.now().hour} - Hour Hex: {hour_to_bin}")
-    print(f"Minute Dec: {datetime.now().minute} - Minute Hex: {minute_to_bin}")
+    # print(f"Hour Dec: {datetime.now().hour} - Hour Hex: {hour_to_bin}")
+    # print(f"Minute Dec: {datetime.now().minute} - Minute Hex: {minute_to_bin}")
 
     ui.binary_clock_hours_label.setText(hour_to_bin)
     ui.binary_clock_minutes_label.setText(minute_to_bin)
